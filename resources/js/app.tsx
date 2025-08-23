c2a25211f1840f33d89641cf5e442ec53a307a7f
Islam Mac <islam@mac.com>
2025-08-23 11:33:45 +0300

    removing primereact

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,41 +4,11 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';
 import './i18n';
-import { PrimeReactProvider } from "@primereact/core";
-import Aura from '@primeuix/themes/aura';
-import { definePreset } from '@primeuix/themes';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-const MyPreset = definePreset(Aura, {
-    semantic: {
-        primary: {
-            50: '{indigo.50}',
-            100: '{indigo.100}',
-            200: '{indigo.200}',
-            300: '{indigo.300}',
-            400: '{indigo.400}',
-            500: '{indigo.500}',
-            600: '{indigo.600}',
-            700: '{indigo.700}',
-            800: '{indigo.800}',
-            900: '{indigo.900}',
-            950: '{indigo.950}'
-        }
-    }
-});
-
-const primereact = {
-    theme: {
-        preset: MyPreset,
-        options: {
-            darkModeSelector: '.dark'
-        }
-    }
-};
 
 createInertiaApp({
     title: (title) => title ? `${title} - ${appName}` : appName,
@@ -47,12 +17,10 @@ createInertiaApp({
         const root = createRoot(el);
 
         root.render(
-            <PrimeReactProvider {...primereact}>
-                <App {...props} />
-            </PrimeReactProvider>
+            <App {...props} />
         );
     },
-    
+
     progress: {
         color: '#4B5563',
     },
